b8cf09ee601c2e1e4b46776f58b39ea9021bd155
Brooke Storm <bstorm@wikimedia.org>
2026-07-02 16:22:07 -0700

    feat: working on the new frontend

---
+++ public/legacy/assets/js/lunr/lunr-store.js
@@ -0,0 +1,76 @@
+var store = [{
+        "title": "A Plywood Box and Why We Want To Live In It",
+        "excerpt":"To begin with, our goal is pretty simple. &nbsp;We want to spend two to five years living a mobile lifestyle while adhering to our four principles of “Independence, Adventure, Sustainability, Freedom” and by doing so free ourselves from previous debt and other baggage. &nbsp;We’ll be blogging about all our adventures...","categories": [],
+        "tags": ["why","caravan"],
+        "url": "/blog/a-plywood-box-and-why-we-want-to-live/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "A Hike on the Cutler Coast in Maine",
+        "excerpt":"&nbsp;These are some photos taken on a relatively grueling yet breathtaking overnight backpacking trip we took in some woods and on cliffs along the Bay of Fundy at the end of July (2014). &nbsp;I cannot possibly describe the beauty and wonder of that place in words and pictures don't do...","categories": [],
+        "tags": ["pics","coast","hiking","backpacking"],
+        "url": "/blog/a-hike-on-cutler-coast-in-maine/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "Learning to Shave With A Straight Razor: A Woman’s Guide",
+        "excerpt":"Sustainability is a central concern we have aboard the Caravan, not just because we care about the planet and the next generation, but because it is something of a necessity in our lifestyle. &nbsp;If we have a lot of trash to throw out, we cannot remain for long in any...","categories": [],
+        "tags": ["bathroom","simple living","shaving"],
+        "url": "/blog/learning-to-shave-with-straight-razor/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "Fun Article for Your Morning",
+        "excerpt":"We can't help but be amused by references to the Oregon Trail game. &nbsp;This article about fairly serious things was no exception from Mental Floss.http://mentalfloss.com/article/28968/where-are-they-now-diseases-killed-you-oregon-trail","categories": [],
+        "tags": ["pioneering","fun"],
+        "url": "/blog/fun-article-for-your-morning/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "A Nautical Diversion",
+        "excerpt":"We had way more fun that we ought to taking a tour of the Half Moon replica while it was in the Albany, NY area!This sort of thing just gets us even more motivated.The ship at dock. Instruments that I want. In order to show everything, they couldn't be perfectly...","categories": [],
+        "tags": ["nautical","fun"],
+        "url": "/blog/a-nautical-diversion/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "Keeping Clean Like a Proper Hobo",
+        "excerpt":"Anyone who has camped in the backwoods understands that when you aren't on a steady well or municipal water supply, keeping clean is a trade-off sometimes. &nbsp;Sure, you can take a shower if you are willing to pay at a truck stop or to stop at a managed park that...","categories": [],
+        "tags": ["bathroom","simple living"],
+        "url": "/blog/keeping-clean-like-proper-hobo/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "Reusables - Handkerchiefs, Rags and Napkins",
+        "excerpt":"Ever look at your trash can and wonder what the heck you would do if you didn't have a garbage truck coming on Friday? &nbsp;We naturally have, especially after spending extended periods living in camp sites. &nbsp;When we really started this whole exercise, it was a kind of horrifying thought...","categories": [],
+        "tags": ["simple living"],
+        "url": "/blog/reusables-handkerchiefs-rags-and-napkins/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "A Glorious Funeral?",
+        "excerpt":"While spending the holidays on the family farm, a mouse and a shrew were found in traps in the cellar. &nbsp;For some reason, these ended up in the care of our daughter, and by the time we heard about this, she had constructed a paper boat and was creating tiny...","categories": [],
+        "tags": ["pics","winter"],
+        "url": "/blog/a-glorious-funeral/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "A man is not dead while his name is still spoken.",
+        "excerpt":"As many of you already know, one of our favorite authors, Sir Terry Pratchett, passed away on March 12th of this year.&nbsp; For those who haven't read Going Postal, in the story, the operators of a primitive telegraphy system (the Clacks) add a coded message to the system overhead that...","categories": [],
+        "tags": ["books","nerdlife"],
+        "url": "/blog/a-man-is-not-dead-while-his-name-is/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "Good times and rough roads in a NY State Forest",
+        "excerpt":"We had a marvelous time during an overnight stay in one of New York's many state forests that was ended with a somewhat harrowing experience.We took a lot of images to share with everyone this time!Our beloved Jeep, Brynhilde, got some mud time!Behold the glory of our camp!Our little one...","categories": [],
+        "tags": ["pics","caravan"],
+        "url": "/blog/good-times-and-rough-roads-in-ny-state/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "Very close!",
+        "excerpt":"We are building! The trailer is nearly ready for pickup, so we are building our new bed for it. &nbsp;It's all so close!Very soon we will be on the longest road trip we've ever had.This might surprise our readers, but it's honestly a bit intimidating as well as thrilling.","categories": [],
+        "tags": ["caravan"],
+        "url": "/blog/very-close/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "Introducing: Boxcar!",
+        "excerpt":"We've been quiet lately because we have been busy acquiring and now building out our new little cargo trailer, which we have named Boxcar. It isn't ready for us to move in yet, but here are some sneak peeks!Boxcar rides!This is what it used to look like. &nbsp;It is already...","categories": [],
+        "tags": ["why","caravan"],
+        "url": "/blog/introducing-boxcar/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "The journey begins!",
+        "excerpt":"We are finally moved out of our apartment and into our little cargo trailer! &nbsp;We cannot possibly express how much work went into this. &nbsp;We didn't own much (or at least we didn't think we did), but the difference between what you \"need\" when living in an apartment vs. what...","categories": [],
+        "tags": ["pics","caravan"],
+        "url": "/blog/the-journey-begins/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "Welcome!",
+        "excerpt":"I’m finally trying to just blog for myself in a sane manner. This round, I’m using Jekyll because I like the name, andbecause I’m annoyed that formatting on hugo changed since I last paid enough attention for it to matter. I am likely to move some content from my various...","categories": ["fluff"],
+        "tags": [],
+        "url": "/blog/fluff/welcome-to-jekyll/",
+        "teaser":"/assets/images/500x300.png"},{
+        "title": "2018 is Finally Almost Over!",
+        "excerpt":"I have to admit that it’s been a rough year. I’ve watched the US become a politicaltrash fire. Loads of things ended up far more costly than we’d hoped in our lovelyplans on paper (and almost ended up total disasters). Through the whole emotional rollercoasterit’s been a net positive for...","categories": ["review"],
+        "tags": [],
+        "url": "/blog/review/finally-done-2018/",
+        "teaser":"/assets/images/thebus-after.jpg"}]
